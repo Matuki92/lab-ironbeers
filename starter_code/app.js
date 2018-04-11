871c26bdd0822f76433057ce8cf2921de41df89f
Matuki <matiaspierretti2@hotmail.com>
2018-04-11 19:48:12 +0200

    iteration 5

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,18 +1,30 @@
 
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+    .then(beers => {
+      console.log(beers)
+      res.render('beers', beers);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
 
-app.listen(3000);
+app.listen(3001, () => console.log('listening on port 3001!'));
